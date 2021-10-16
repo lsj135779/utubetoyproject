f12aab937f9792837d30ad3a93933ba9b4aaa601
anniemon <seojung19@gmail.com>
2021-10-17 01:54:55 +0900

    assets에 더미 비디오 폴더 생성, react-player로 더미 비디오 1개 테스트 플레이

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
+import ReactPlayer from 'react-player';
+import { dummyVideos } from '../assets/videos'
+import styled from 'styled-components';
+
 
 export default function Video () {
+
   return (
     <div>
       <Header />
+      <section>
+      <ReactPlayer 
+        width='1000px' 
+        height='562.5px' 
+        controls 
+        url={dummyVideos.videos[0].src}
+      />
+      </section>
     </div>
   );
 }
