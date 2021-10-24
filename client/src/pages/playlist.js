2981ba7ad16620a224b81e3b9838da1f42400396
이승준 <lsj135779@gmail.com>
2021-10-24 16:43:04 +0900

    footer 생성, 영역설정 upload페이지 틀 생성

--- client/src/pages/playlist.js
+++ client/src/pages/playlist.js
@@ -1,40 +1,51 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-import Header from "../components/Header";
-import Thumbnail from "../components/Thumbnail";
+import styled from 'styled-components';
+import Header from '../components/Header';
+import Thumbnail from '../components/Thumbnail'
+import Footer from '../components/Footer';
 
-const Title = styled.div`
-  margin: 30px 50px 0px 50px;
-  font-size: 30px;
-  font-weight: bold;
+
+const Wrap = styled.div`
+  min-height: 100vh;
+  position: relative;
+  width: 100%;
+`;
+
+const Body = styled.div`
+  padding-bottom: 100px;
+  h1{
+    margin: 10px 50px 0 50px;
+  }
 `;
 
 const Playlist = styled.div`
-  border-top: 1px solid;
+	border-top: 1px solid;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: flex-start;
   margin: 10px 50px 0 50px;
-  .link {
-    text-decoration: none;
-    color: black;
-  }
 `;
 
-export default function PlayList ({ imgs, handleClick, clicked }) {
+
+export default function PlayList ({ imgs, handleClick }) {
+
 
   return (
-    <div>
-      <Header handleClick={handleClick} />
-      <Title>Recommended</Title>
-      <Playlist>
-        { imgs.map(thumbnail => 
-          <Link to="/play" key={thumbnail.id} className="link">
-          <Thumbnail key={thumbnail.id} clicked={clicked} thumbnail={thumbnail} handleClick={handleClick}/>
-          </Link> )}
-      </Playlist>
-    </div>
+    <Wrap>
+      <Header />
+      <Body>
+        <br/>
+        <h1>Recommended</h1>
+        <Playlist>
+          { imgs.map(thumbnail => 
+            <Link to="/play">
+              <Thumbnail key={thumbnail.id} thumbnail={thumbnail} handleClick={handleClick}/>
+            </Link> )}
+        </Playlist>
+      </Body>
+      <Footer />
+    </Wrap>
   );
 }
